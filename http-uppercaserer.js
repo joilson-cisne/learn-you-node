b7dcaed3e954adbc00b568ebf88308a1361417ed
Joilson Cisne <joilsoncisne@gmail.com>
2017-03-26 03:07:35 -0300

    Complete exercise 12

---
+++ http-uppercaserer.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var map = require('through2-map');
+
+var port = process.argv[2];
+
+var server = http.createServer((req, res) => {
+  req.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(res);
+});
+
+server.listen(port);
+
