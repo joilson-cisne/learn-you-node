652794261f17b53cea48259f10147e800577684d
Joilson Cisne <joilsoncisne@gmail.com>
2017-03-26 02:54:00 -0300

    Complete exercise 11

---
+++ http-file-server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+var fs = require('fs');
+
+var port = process.argv[2];
+var filePath = process.argv[3];
+
+var server = http.createServer((req, res) => {
+  fs.createReadStream(filePath).pipe(res);
+});
+
+server.listen(port);
