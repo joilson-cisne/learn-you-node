3415112d8eb576f6808b7312483442570a26594d
Joilson Cisne <joilsoncisne@gmail.com>
2017-03-26 12:28:49 -0300

    Refactor api server

--- api-server.js
+++ api-server.js
@@ -3,25 +3,38 @@ var url = require('url');
 
 var port = process.argv[2];
 
+function parsetime(date) {
+  return {
+    'hour': date.getHours(),
+    'minute': date.getMinutes(),
+    'second': date.getSeconds(),
+  };
+}
+
+function unixtime(date) {
+  return {
+      'unixtime': date.getTime(),
+    };
+}
+
 var server = http.createServer((req, res) => {
   var parsedUrl = url.parse(req.url, true);
   var iso = parsedUrl.query.iso;
   var date = new Date(iso);
+  var result;
   
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-
   if (parsedUrl.pathname == '/api/parsetime') {
-    var result = {
-      'hour': date.getHours(),
-      'minute': date.getMinutes(),
-      'second': date.getSeconds(),
-    }
-    res.end(JSON.stringify(result));
+    result = parsetime(date);
   } else if (parsedUrl.pathname == '/api/unixtime') {
-    var result = {
-      'unixtime': date.getTime(),
-    }
+    result = unixtime(date);
+  }
+
+  if (result) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify(result));
+  } else {
+    res.writeHead(404);
+    res.end();
   }
 });
 
